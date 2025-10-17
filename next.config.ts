bcaa2e793cc18968ae82773a59dff1dfb68360c9
Vinicius Barbieri <vinicius190702@hotmail.com>
2025-10-17 11:03:32 -0300

    fix: atualiza CORS settings

--- next.config.ts
+++ next.config.ts
@@ -15,11 +15,13 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: process.env.FRONTEND_URL || "http://localhost:3000",
+            // value: process.env.FRONTEND_URL || "http://localhost:3000",
+            // para desenvolvimento, permitimos todas as origens
+            value: "*",
           }, // Domínio do seu frontend
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
